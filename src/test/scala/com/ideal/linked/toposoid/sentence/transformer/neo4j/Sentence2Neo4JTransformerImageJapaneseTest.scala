e53aeba41bce248aa2099ce5b743c77aa0746fba
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2024-08-25 12:21:12 +0900

    https://github.com/toposoid/toposoid-sentence-transformer-neo4j/issues/164

--- src/test/scala/com/ideal/linked/toposoid/sentence/transformer/neo4j/Sentence2Neo4JTransformerImageJapaneseTest.scala
+++ src/test/scala/com/ideal/linked/toposoid/sentence/transformer/neo4j/Sentence2Neo4JTransformerImageJapaneseTest.scala
@@ -27,7 +27,7 @@ import org.neo4j.driver.Result
 
 class Sentence2Neo4JTransformerImageJapaneseTest extends AnyFlatSpec with BeforeAndAfter with BeforeAndAfterAll {
 
-  val transversalState:TransversalState = TransversalState(username="guest")
+  val transversalState:TransversalState = TransversalState(userId="test-user", username="guest", roleId=0, csrfToken = "")
 
   before {
     Neo4JAccessor.delete()
